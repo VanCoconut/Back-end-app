111236f113fb2633e3e64d0cc9d7ec9b3f3e3626
Salvatore Scalisi <scalisi.salvo02@gmail.com>
2023-06-16 09:53:55 +0200

    fix ProductRepository

--- src/main/java/com/lipari/app/products/repositories/ProductRepository.java
+++ src/main/java/com/lipari/app/products/repositories/ProductRepository.java
@@ -2,7 +2,9 @@ package com.lipari.app.products.repositories;
 
 import com.lipari.app.products.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 
     boolean existsById(Integer id);
